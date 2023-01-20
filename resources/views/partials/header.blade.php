fae8d011a29791274f7de1a08a6c06a59be594c6
PiersilvioPellegrini <pier.pellegrini.23@gmail.com>
2023-01-20 16:24:59 +0100

    first commit

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,29 @@
+
+
+<section class="bg-primary">
+    <div class="container">
+        <div class="smallNavbar d-flex justify-content-end text-white gap-4 p-1 text-center">
+            <p class="mb-0 fw-bold">DC POWER VISA</p>
+            <p class="mb-0 fw-bold">ADDITIONAL DC SITES</p>
+        </div>
+    </div>
+</section>
+<section>
+    <div class="container">
+        <nav class="d-flex justify-content-between align-items-center pt-3">
+            <div class="logo ">
+                <img src="{{Vite::asset("resources/imgs/dc-logo.png")}}" alt="">
+            </div>
+            <div class="mainNav">
+                <ul>
+                    <li></li>
+                </ul>
+    
+            </div>
+            <div class="searchBar">
+            </div>
+        </nav>
+
+    </div>
+</section>
+
